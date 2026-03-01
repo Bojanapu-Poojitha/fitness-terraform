6e07583b64e39f84451c6cfcfec209189a8b8885
Bojanapu-Poojitha <poojitha.b@everest.engineering>
2026-03-01 23:01:34 +0530

    Add app port and ssh inbound rule

--- main.tf
+++ main.tf
@@ -66,6 +66,18 @@ resource "aws_security_group" "ec2_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+   ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 }
