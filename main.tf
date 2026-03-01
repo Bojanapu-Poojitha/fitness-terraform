6c7ae90152aa8dbe9f75b7974714a0aaa026c20a
Bojanapu-Poojitha <poojitha.b@everest.engineering>
2026-03-01 22:50:00 +0530

    Add a vpc along with cidr block and enable dns

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 provider "aws"{
     region = var.aws_region
 }
+
+resource "aws_vpc" "fitness_vpc"{
+    cidr_block          ="10.0.0.0/16"
+    enable_dns_hostname =true
+    tags ={
+        Name="fitness-vpc"
+    }
+}
