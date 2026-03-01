f29a143d47b3e39362a23b640f27654cb8fb92b0
Bojanapu-Poojitha <poojitha.b@everest.engineering>
2026-03-01 22:58:33 +0530

    Create a route table and link igw

--- main.tf
+++ main.tf
@@ -42,4 +42,15 @@ resource "aws_internet_gateway" "fitness_igw"{
         Name = "fitness-igw"
     }
 }
+
+resource "aws_route_table" "public_rt"{
+    vpc_id = aws_vpc.fitness_vpc.id
+    route{
+          cidr_block = "0.0.0.0/0"
+         gateway_id = aws_internet_gateway.fitness_igw.id
+    }
+    tags ={
+        Name = "fitness-public-rt"
+    }
+}
 }
