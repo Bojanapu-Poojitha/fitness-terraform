75c0d78643931f008c15d621aeb98d0e926d2429
Bojanapu-Poojitha <poojitha.b@everest.engineering>
2026-03-01 23:02:22 +0530

    Implement outbound rule and give security group name

--- main.tf
+++ main.tf
@@ -78,6 +78,13 @@ resource "aws_security_group" "ec2_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+   egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    tags = { Name = "fitness-ec2-sg" }
 }
 
 }
