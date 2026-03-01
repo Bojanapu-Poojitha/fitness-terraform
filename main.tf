e79bd148cfcfdc962c4748e8e33616c888042914
Bojanapu-Poojitha <poojitha.b@everest.engineering>
2026-03-01 22:54:44 +0530

    Implement a two private subnets with different availability zones

--- main.tf
+++ main.tf
@@ -18,4 +18,21 @@ resource "aws_subnet" "public_subnet"{
         Name = "fitness-public-subnet"
     }
 }
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.fitness_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "${var.aws_region}a"
+  tags =
+   {
+     Name = "fitness-private-subnet-1"
+     }
+}
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.fitness_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "${var.aws_region}b"
+  tags = {
+     Name = "fitness-private-subnet-2"
+      }
+}
 }
