5d49fb30d81443cadcf74c05e4cf040947032cb2
Bojanapu-Poojitha <poojitha.b@everest.engineering>
2026-03-02 10:25:41 +0530

    Create a IAM role for lambda function

--- main.tf
+++ main.tf
@@ -136,6 +136,17 @@ resource "aws_s3_bucket_policy" "thumbnails_policy" {
     }]
   })
 }
+resource "aws_iam_role" "lambda_role" {
+  name = "fitness-lambda-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
 
 resource "aws_security_group" "rds_sg" {
   name   = "fitness-rds-sg"
