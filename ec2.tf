6c24d9bdbc8a720df1cdb348bebb280463b41e6c
Bojanapu-Poojitha <poojitha.b@everest.engineering>
2026-03-05 14:29:29 +0530

    Place ec2 code in ec2 file with inbound and outbound

---
+++ ec2.tf
@@ -0,0 +1,72 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+}
+resource "aws_security_group" "ec2_sg" {
+  name   = "fitness-ec2-sg"
+  vpc_id = aws_vpc.fitness_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "fitness-ec2-sg" }
+}
+
+resource "aws_instance" "fitness_ec2" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  key_name               = var.key_pair_name
+
+  tags = {
+    Name = "fitness-ec2"
+  }
+}
+
+resource "aws_eip" "fitness_eip" {
+  instance = aws_instance.fitness_ec2.id
+  domain   = "vpc"
+
+  tags = {
+    Name = "fitness-eip"
+  }
+}
