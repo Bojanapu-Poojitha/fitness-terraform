f06fa3ad33b74a4b70a24fe1a3c7ab3c248a630d
Bojanapu-Poojitha <poojitha.b@everest.engineering>
2026-03-05 14:31:10 +0530

    Create a vpc file and place igw and subnets

---
+++ vpc.tf
@@ -0,0 +1,65 @@
+resource "aws_vpc" "fitness_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "fitness-vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.fitness_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "fitness-public-subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.fitness_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "${var.aws_region}a"
+
+  tags = {
+    Name = "fitness-private-subnet-1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.fitness_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "${var.aws_region}b"
+
+  tags = {
+    Name = "fitness-private-subnet-2"
+  }
+}
+
+resource "aws_internet_gateway" "fitness_igw" {
+  vpc_id = aws_vpc.fitness_vpc.id
+
+  tags = {
+    Name = "fitness-igw"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.fitness_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.fitness_igw.id
+  }
+
+  tags = {
+    Name = "fitness-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public_rta" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
